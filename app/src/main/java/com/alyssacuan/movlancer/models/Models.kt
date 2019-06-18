1ea1f83b93e343066f4497304055654549e5ad2c
Alyssa-Cuan <alyssa.cuan@obf.ateneo.edu>
2019-06-18 16:51:27 +0800

    Added Paging dependencies

--- app/src/main/java/com/alyssacuan/movlancer/models/Models.kt
+++ app/src/main/java/com/alyssacuan/movlancer/models/Models.kt
@@ -1,12 +1,16 @@
 package com.alyssacuan.movlancer.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "movie_table")
 data class Movie(
-    @SerializedName("id") val id: Int,
-    @SerializedName("title") val title: String,
-    @SerializedName("vote_average") val voteAverage: Double,
-    @SerializedName("poster_path") val posterPath: String
+    @PrimaryKey @ColumnInfo(name = "id") @SerializedName("id") val id: Int,
+    @ColumnInfo(name = "title") @SerializedName("title") val title: String,
+    @ColumnInfo(name = "vote_average") @SerializedName("vote_average") val voteAverage: Double,
+    @ColumnInfo(name = "poster_path") @SerializedName("poster_path") val posterPath: String
 )
 
 data class Result (val page: Int, val total_results: Int, val total_pages: Int, val results: List<Movie>)
